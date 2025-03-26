2e756d7da916a0ad6175582628aca476053ce009
Caio Miranda Pereira <71469917+caiowmp@users.noreply.github.com>
2025-03-26 23:28:06 +0000

    cria o componente `Temporizador.vue`

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+    };
+  },
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      clearInterval(this.cronometro);
+    },
+  },
+  components: { Cronometro },
+});
+</script>
