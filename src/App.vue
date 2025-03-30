03120aa3b8748fde8fd5d405026ec8bdcee41b2c
caiomp <caiomp@ufba.br>
2025-03-30 13:30:36 -0300

    adiciona método de troca de tema

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,55 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
+      <Formulario @aoSalvarTarefa="salvarTarefa"/>
       <div class="lista">
-        <Tarefa
-          v-for="(tarefa, index) in tarefas"
-          :key="index"
-          :tarefa="tarefa"
-        />
-        <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje :( </Box>
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+        <Box v-if="listaEstaVazia">
+          Você não está muito produtivo hoje :(
+        </Box>
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import BarraLateral from "./components/BarraLateral.vue";
-import Formulario from "./components/Formulario.vue";
-import Tarefa from "./components/Tarefa.vue";
-import Box from "./components/Box.vue";
-import ITarefa from "./interfaces/ITarefa";
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
+import Tarefa from './components/Tarefa.vue'
+import Box from './components/Box.vue'
+import ITarefa from './interfaces/ITarefa'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     BarraLateral,
     Formulario,
     Tarefa,
-    Box,
+    Box
   },
-  data() {
+  data () {
     return {
       tarefas: [] as ITarefa[],
-    };
+      modoEscuroAtivo: false
+    }
   },
   computed: {
-    listaEstaVazia(): boolean {
-      return this.tarefas.length === 0;
-    },
+    listaEstaVazia () : boolean {
+      return this.tarefas.length === 0
+    }
   },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa);
+    salvarTarefa (tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
     },
-  },
+    trocarTema (modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
+    }
+  }
 });
 </script>
 
