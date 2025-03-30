e3cc93b85413988088f01dc8684d2b8b2f3e6bac
Caio Miranda Pereira <71469917+caiowmp@users.noreply.github.com>
2025-03-30 18:24:51 +0000

    adiciona referencia ao router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+import roteador from "./roteador";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).use(roteador).mount("#app");
